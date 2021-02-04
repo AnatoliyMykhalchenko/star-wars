c8be45cdd4705bba7118f2c0b839984d477a65a8
AnatoliyMykhalchenko <anatoliymykhalchenko@gmail.com>
2021-02-05 00:39:06 +0200

    move residents to separate component

--- src/app/planet/planet.module.ts
+++ src/app/planet/planet.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { PlanetComponent } from './planet.component';
+import { ResidentsComponent } from './residents/residents.component';
 
 @NgModule({
   declarations: [
     PlanetComponent,
+    ResidentsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/planet/residents/residents.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-residents',
+  templateUrl: './residents.component.html',
+  styleUrls: ['./residents.component.scss'],
+})
+export class ResidentsComponent implements OnInit {
+  @Input() residents;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
