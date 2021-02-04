e4c3654db06d13dfb329a226c0df76d4b40617be
AnatoliyMykhalchenko <anatoliymykhalchenko@gmail.com>
2021-02-04 22:51:43 +0200

    added default router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanetsComponent } from './planets/planets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'planets',
+    component: PlanetsComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'planets',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
